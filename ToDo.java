7b27eaa10869f1adb60fb6c28fbcf4b50c8e675d
ahetric <55210657+ahetric@users.noreply.github.com>
2020-03-14 16:49:54 -0400

    Update ToDo.java

--- ToDo.java
+++ ToDo.java
@@ -1,9 +1,18 @@
 public class ToDo {
 
+
+- get things back to normal (if gradient or ! box visible, automatically set to normal if any other button clicked)
+- gradient on top, boxes on bottom
+
+- make sure gradient is below alert/close button ?
+
+-make notification if color mod and wraps around (fix the non-mod number, like the (400,425,300) type thing)
+
 - choose pretty default colors (add dice option to shuffle initial colors) [fix efficiency of this]
 - add a "get complement" option, which keeps initial color same and sets midpoint to #808080
 
 - find a way to deal w weird combos (mod or force to 0xff or 0x00)? [only choose 'good' starting values for shuffle, like within margin]
+     - user mode (change to enum)
 
 - deal with square label font size and overall window size adjustment
 - add instructions button at the bottom (or maybe menu)
@@ -11,5 +20,20 @@ public class ToDo {
 - make it into a jar?
 
 -when done w colorJumper_Swatches and ColorPickerSample, delete HexColorJumper's getR1_int(), getR2_int(), getRfinal()
+
+
+
+
+https://stackoverflow.com/questions/48966170/is-it-possible-to-get-the-new-color-in-javafx-colorpicker
+https://stackoverflow.com/questions/45966844/how-to-change-the-icon-size-of-a-color-picker-in-javafx
+gpx
+http://tutorials.jenkov.com/javafx/button.html
+http://tutorials.jenkov.com/javafx/group.html
+http://tutorials.jenkov.com/javafx/overview.html
+http://www.java2s.com/Tutorials/Java/JavaFX/0300__JavaFX_HBox.htm
+https://docs.oracle.com/javase/8/javafx/api/javafx/scene/Node.html#setTranslateX-double-
+https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/Region.html#maxWidthProperty
+https://docs.oracle.com/javafx/2/ui_controls/color-picker.htm#BABJGJJA
+
 
 }
